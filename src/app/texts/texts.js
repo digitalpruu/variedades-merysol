a53471a2edc7b99ed460381d2bf836600f7a7e5a
kevinpicon01 <digitalpruu@gmail.com>
2024-10-01 00:51:17 -0500

    Changed media, color, texts

--- src/app/texts/texts.js
+++ src/app/texts/texts.js
@@ -1,9 +1,9 @@
-const title = "Titulo del emprendimiento";
-const email = "Email del emprendimiento";
+const title = "Variedades Merysol";
+const email = "merisol.gomez33@gmail.com";
 const mensajeWhatsApp = encodeURIComponent(
-    `¡Hola! Estoy muy interesado en tus productos de ${title} y me gustaría recibir más información sobre ellos. Espero poder hablar pronto contigo para aclarar algunas dudas. ¡Gracias!`
+    `¡Hola! Estoy interesado en los productos de ${title} y me gustaría recibir más información sobre la mercancía para el hogar y ropa deportiva. ¡Espero su respuesta!`
 );
-const numeroWhatsApp = "Numero del emprendimiento";
+const numeroWhatsApp = "3209142666";
 const textos = {
     index: {
         title: title,
@@ -12,25 +12,24 @@ const textos = {
         title: title,
     },
     about: {
-        description1: "Descripción del emprendimiento parrafo uno",
-        description2: "Descripcion del emprendimiento parrafo dos"
+        description1: "Variedades Merysol se especializa en la compra y venta de mercancía para el hogar, ofreciendo una amplia gama de productos como sábanas, toallas, edredones, cobijas térmicas y de peluche.",
+        description2: "También contamos con ropa deportiva, incluyendo joggers, camisetas, leggings y sudaderas, todo diseñado para brindar confort y estilo a nuestros clientes.",
     },
     contact: {
         whatsappLink: `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`,
-        email: email
+        email: email,
     },
     footer: {
-        slogan:{
-            p1: "slogan del emprendimiento parrafo uno",
-            p2: "slogan del emprendimiento parrafo dos"
+        slogan: {
+            p1: "Todo para el hogar",
+            p2: "Comodidad y estilo en un solo lugar",
         },
-        address: "Dirección del emprendimiento",
+        address: "",
         phone: numeroWhatsApp,
         email: email,
-        facebook: "https://www.facebook.com/",
-        instagram: "https://www.instagram.com/",
-        whatsappLink: `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`
+        facebook: "https://www.facebook.com/MerysolGomezMelo", // Facebook proporcionado.
+        instagram: "https://www.instagram.com/MerysolGomezMelo", // Instagram proporcionado.
+        whatsappLink: `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`,
     },
-
 };
 export default textos;
